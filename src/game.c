d27855d6fca2a7b1181c3584379a289f8ef39ccc
MgShepherd <michaelshep1504@gmail.com>
2025-09-25 11:01:42 +0100

    Setup loading player texture from spritesheet

--- src/game.c
+++ src/game.c
@@ -2,34 +2,66 @@
 #include <raylib.h>
 #include <stdio.h>
 
-#define WIDTH 800
-#define HEIGHT 600
+static const float WIDTH = 800.0f;
+static const float HEIGHT = 600.0f;
+static const float PLAYER_Y_POS = 500.0f;
+static const float PLAYER_SIZE = 32.0f;
+static const int IMAGE_SIZE = 16;
 
-void m_game_handle_input(Vector2 *player_pos) {
-  if (IsKeyDown(KEY_A))
-    player_pos->x -= 200 * GetFrameTime();
-  else if (IsKeyDown(KEY_D))
-    player_pos->x += 200 * GetFrameTime();
-}
+static const char *SPRITESHEET_PATH = "resources/SpriteSheet.png";
 
-void m_game_loop() {
-  Vector2 player_pos = {500, 500};
+typedef struct {
+  Rectangle image_region;
+  Rectangle bounds;
+} M_GameObject;
+
+void m_game_loop(Texture2D *sprite_sheet, M_GameObject *player) {
   while (!WindowShouldClose()) {
-    m_game_handle_input(&player_pos);
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
+    ClearBackground(BLACK);
 
     DrawFPS(10, 10);
-    DrawRectangleV(player_pos, (Vector2){32, 32}, RED);
+    DrawTexturePro(*sprite_sheet, player->image_region, player->bounds,
+                   (Vector2){.x = 0.0f, .y = 0.0f}, 0.0f, WHITE);
 
     EndDrawing();
   }
 }
 
+void m_game_load_objects(M_GameObject *player) {
+  player->image_region = (Rectangle){.x = 0 * IMAGE_SIZE,
+                                     .y = 0 * IMAGE_SIZE,
+                                     .width = IMAGE_SIZE,
+                                     .height = IMAGE_SIZE};
+  player->bounds = (Rectangle){.x = WIDTH / 2 - PLAYER_SIZE / 2,
+                               .y = PLAYER_Y_POS,
+                               .width = PLAYER_SIZE,
+                               .height = PLAYER_SIZE};
+}
+
+int m_game_start() {
+  Texture2D sprite_sheet = LoadTexture(SPRITESHEET_PATH);
+  if (sprite_sheet.id <= 0) {
+    // TODO: Improve error system
+    printf("Unable to load texture %s\n", SPRITESHEET_PATH);
+    return 1;
+  }
+
+  M_GameObject player;
+  m_game_load_objects(&player);
+
+  m_game_loop(&sprite_sheet, &player);
+
+  UnloadTexture(sprite_sheet);
+  return 0;
+}
+
 void m_game_run() {
-  InitWindow(WIDTH, HEIGHT, "My Window");
   SetConfigFlags(FLAG_VSYNC_HINT);
-  m_game_loop();
+  InitWindow(WIDTH, HEIGHT, "Space Invaders");
+
+  m_game_start();
+
   CloseWindow();
 }
